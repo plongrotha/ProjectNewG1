eccd98ff23d0f86d1d7742bd65492fabcc0c5e68
C-vd-twenty6 <vandy5144@gmail.com>
2024-11-04 13:45:22 +0700

    new

---
+++ src/app/Temclose/temclose.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'temclose',
+  templateUrl: './temclose.component.html',
+  styleUrl: './temclose.component.css',
+})
+export class temclose {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Admin } from './admin/admin.component';
 import { Link } from './link/link.component';
+import { temclose } from './Temclose/temclose.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,7 @@ import { Link } from './link/link.component';
     Payment,
     Admin,
     Link,
-    
+    temclose,
   ],
   imports: [
     BrowserModule,
