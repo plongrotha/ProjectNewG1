ecefa3c47d9b786d4ee9dd6da17c0e6e800ee4ae
plong rotha <Mrr.rothabetta31@gmail.com>
2024-11-04 09:46:03 +0700

    delete form

--- src/app/model/Employee.ts
+++ src/app/model/Employee.ts
@@ -1,27 +1,26 @@
 export class EmployeeModel {
-    empId: number;
-    name: string;
-    city: string;
-    state: string;
-    emailId: string;
-    contactNo: number;
-    address: string;
-    add: string;
-    pinCode: number;
-    country: string;
-  
-    constructor() {
-      this.address = '';
-      this.add = '';
-      this.city = '';
-      this.contactNo = 0;
-      this.emailId = '';
-      this.empId = 1;
-      this.name = '';
-  
-      this.pinCode = 0;
-      this.state = '';
-      this.country = '';
-    }
+  empId: number;
+  name: string;
+  city: string;
+  state: string;
+  emailId: string;
+  contactNo: number;
+  address: string;
+  add: string;
+  pinCode: number;
+  country: string;
+
+  constructor() {
+    this.address = '';
+    this.add = '';
+    this.city = '';
+    this.contactNo = 0;
+    this.emailId = '';
+    this.empId = 1;
+    this.name = '';
+
+    this.pinCode = 0;
+    this.state = '';
+    this.country = '';
   }
-  
+}
