0bbf113e11919b18863f397b535e683a78c7399f
C-vd-twenty6 <vandy5144@gmail.com>
2024-11-04 09:43:50 +0700

    sasa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppComponent } from './app.component';
 import { Card } from './card/card.component';
 import { Header } from './header/header.component';
 import { Menu } from './header/menu/menu.component';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Home } from './home/home.component';
 import { Serivce } from './service/service.component';
 import { Payment } from './payment/payment.component';
 import { EmployeeModel } from './model/Employee';
 import { FirstForm } from './firstform/firstform.component';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,10 +27,19 @@ import { FirstForm } from './firstform/firstform.component';
     Home,
     Serivce,
     Payment,
-    EmployeeModel,
+
     FirstForm,
   ],
-  imports: [BrowserModule, AppRoutingModule, RouterLink, RouterLinkActive],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterLink,
+    RouterLinkActive,
+    RouterOutlet,
+    ReactiveFormsModule,
+    CommonModule,
+    RouterLink,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
