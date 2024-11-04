7e98abeed578a17ca2ed9a883767854ce4458af9
C-vd-twenty6 <vandy5144@gmail.com>
2024-11-04 15:46:09 +0700

    yes

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,8 +1,59 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+interface User {
+  city: any;
+  state: any;
+  country: any;
+  address1: any;
+  address2: any;
+  phone: any;
+  email: any;
+  name: string;
+  age: number;
+}
 
 @Component({
   selector: 'admin',
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
-export class Admin {}
+export class Admin {
+  myForm: FormGroup;
+  dataSource: User[] = [];
+  editIndex: number | null = null;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      email: ['', Validators.required],
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      address1: ['', Validators.required],
+      address2: ['', Validators.required],
+      country: ['', Validators.required],
+      state: ['', Validators.required],
+      city: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      if (this.editIndex !== null) {
+        this.dataSource[this.editIndex] = this.myForm.value;
+        this.editIndex = null; // Reset edit index
+      } else {
+        this.dataSource.push(this.myForm.value);
+      }
+      this.myForm.reset();
+    }
+  }
+
+  onEdit(index: number) {
+    this.editIndex = index;
+    this.myForm.patchValue(this.dataSource[index]);
+  }
+
+  onDelete(index: number) {
+    this.dataSource.splice(index, 1);
+  }
+}
